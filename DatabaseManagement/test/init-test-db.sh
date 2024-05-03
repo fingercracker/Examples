866f502d4d7959471a122bbb430579dd66ce5caf
hypothesis <jmart.will@gmail.com>
2024-05-03 14:08:33 -0600

    initialize database script, run tests, added test for arm plug

--- DatabaseManagement/test/init-test-db.sh
+++ DatabaseManagement/test/init-test-db.sh
@@ -41,4 +41,22 @@ psql -v ON_ERROR_STOP=1 --username test_user --dbname postgres <<-EOSQL
         ('H2', 'W01', 'ABC1234', 'SOME_SIGNAL_NAME_1', 'White22AWG', 'H2-H1-J1', '2', 'RWA1-P2', '10', 'Multiple'),
         ('H2', 'W01', 'ABC1234', 'SOME_SIGNAL_NAME_2', 'Blue24AWG', 'H2-H1-J1', '4', 'RWA1-P2', '20', 'Multiple');
 
+    create table h3(
+        harness text,
+        reference text,
+        mark text,
+        net text,
+        wire text,
+        source text,
+        pin text,
+        target text,
+        pin_1 text,
+        manufacturer text
+    );
+
+    insert into h3 (harness, reference, mark, net, wire, source, pin, target, pin_1, manufacturer)
+    values 
+        ('H3', 'W01', 'ABC1234', 'SOME_SIGNAL_NAME_3', 'White22AWG', 'XFC-ARM-J1', '2', 'XFC-P1', '10', 'Multiple'),
+        ('H3', 'W01', 'ABC1234', 'SOME_SIGNAL_NAME_3', 'White22AWG', 'FTSU-P09', '4', 'XFC-ARM-J1', '20', 'Multiple');
+
 EOSQL
--- DatabaseManagement/test/test_generate_report.py
+++ DatabaseManagement/test/test_generate_report.py
@@ -1,20 +1,16 @@
 from DatabaseManagement.utils.database_handler import get_conn, select_with_where
 from DatabaseManagement.jobs.generate_report import process_record
 
-conn = get_conn(
-    host="0.0.0.0",
-    dbname="postgres",
-    user="test_user",
-    password="test_password_1234"
-)
+def test_process_record_interconnect():
+    """
+        Table h1 is a source table while h2 contains an interconnect as a source connector.
 
-def test_process_record():
-    recs = select_with_where(
-        conn,
-        table_name="h1",
-        wheres={
-            "net": ("=", "SOME_SIGNAL_NAME_1")
-        }
+    """
+    conn = get_conn(
+        host="0.0.0.0",
+        dbname="postgres",
+        user="test_user",
+        password="test_password_1234"
     )
 
     table_recs = select_with_where(
@@ -23,6 +19,14 @@ def test_process_record():
         cols=["table_name"],
         wheres={
             "table_schema": ("=", "public")
+        }
+    )
+
+    recs = select_with_where(
+        conn,
+        table_name="h1",
+        wheres={
+            "net": ("=", "SOME_SIGNAL_NAME_1")
         }
     )
 
@@ -49,3 +53,57 @@ def test_process_record():
         'SOME_SIGNAL_NAME_1', 'H1', 'PCUSC1-P1', '1', 'H1', 'H2-H1-P1', '2', 'H2', 'H2-H1-J1', '2', 'H2', 'RWA1-P2', '10'
     ]
 
+def test_process_record_arm_plug():
+    """
+        test table h3 has an arm plug with a single signal going through that arm plug. Verify
+        that the signal self joins correctly
+    """
+    conn = get_conn(
+        host="0.0.0.0",
+        dbname="postgres",
+        user="test_user",
+        password="test_password_1234"
+    )
+
+    table_recs = select_with_where(
+        conn,
+        table_name="information_schema.tables",
+        cols=["table_name"],
+        wheres={
+            "table_schema": ("=", "public")
+        }
+    )
+
+    recs = select_with_where(
+        conn,
+        table_name="h3",
+        wheres={
+            "net": ("=", "SOME_SIGNAL_NAME_3")
+        }
+    )
+
+    table_names = [x["table_name"] for x in table_recs]
+    target_names = [x["target"] for x in recs]
+    source_names = [x["source"] for x in recs]
+    source_only_recs = [x for x in recs if x["source"] not in target_names]
+    
+    rec = source_only_recs[0]
+
+    cols = [
+        "harness",
+        "mark",
+        "wire",
+        "net",
+        "source",
+        "target",
+        "pin", 
+        "pin_1"
+    ]
+
+    processed_records = process_record(conn, rec, source_names, table_names, cols)
+
+    conn.close()
+
+    assert processed_records == [
+        'SOME_SIGNAL_NAME_3', 'H3', 'FTSU-P09', '4', 'H3', 'XFC-ARM-J1', '20', 'H3', 'XFC-ARM-J1', '2', 'H3', 'XFC-P1', '10'
+    ]
